eefea970ce3154402abf67971503d9c1aa5429d0
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-01-02 11:17:02 +0530

    g

---
+++ index.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+
+//     char one='a';
+   
+//     do{
+//         printf
+//         ("%c\n",one);
+//         one+=4;
+//     }
+// while(one<='z');
+
+
+//2 que
+
+{
+    int num=50;
+    int k=20;
+    int sum;
+    for(int i=1; 
+    i<num;
+    i++){
+        if(i%k==0){
+            sum+=i;
+        }
+        
+    }
+
+printf("%d",sum);
+    return 0;
+}
